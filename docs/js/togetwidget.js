8697c6275fc3a942a6456d5f8640ed0b5b099d07
khasenovweb <hasenovkirill93@gmail.com>
2021-06-04 18:07:57 +0500

    dkfjhdkfjh

--- docs/js/togetwidget.js
+++ docs/js/togetwidget.js
@@ -1,13 +1,13 @@
 // Подключаем Jquery если его нет
 if(!window.jQuery){
-    document.write('<script type="text/javascript" src="https://cdn.togett.ru/js/jquery.js"></script>')
+    document.write('<script src="https://cdn.togett.ru/js/jquery.js"></script>')
 }
 // END Подключаем Jquery если его нет
 
 // Подключаем JS
-document.write('<script type="text/javascript" src="https://cdn.togett.ru/js/hammer.js"></script>')
-document.write('<script type="text/javascript" src="https://cdn.togett.ru/js/mobile-detect.js"></script>')
-document.write('<script type="text/javascript" src="https://cdn.togett.ru/js/main.js"></script>')
+document.write('<script src="https://cdn.togett.ru/js/hammer.js"></script>')
+document.write('<script src="https://cdn.togett.ru/js/mobile-detect.js"></script>')
+document.write('<script src="https://cdn.togett.ru/js/main.js"></script>')
 // END Подключаем JS
 // Подключаем CSS
 document.write('<link rel="stylesheet" href="https://cdn.togett.ru/css/togetwidget.css">')
