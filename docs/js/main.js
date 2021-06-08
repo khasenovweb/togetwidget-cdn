b092ddefae75be6a3edf5df91c231a96d569c9ba
khasenovweb <hasenovkirill93@gmail.com>
2021-06-08 19:46:28 +0500

    dflkjhdfkjh

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,24 +1,38 @@
 $(document).ready(function(){
-    
+
     var detect = new MobileDetect(window.navigator.userAgent);
     if( detect.mobile() != null ) {
 
+        var domain = location.hostname; 
+
         $.ajax({
-            url: 'https://cdn.togett.ru/',
+            url: 'https://togett.ru/'+domain,
             type: 'get',
             success: function(data) {
-                $('body').append(data);
+                if( data != 'null' ) {
+                    $.ajax({
+                        url: 'https://togett.ru/panel/'+data,
+                        type: 'get',
+                        success: function(data) {
+                            $('body').append(data);
+                        }
+                    });
+                
+                
+                    $.ajax({
+                        url: 'https://togett.ru/button/'+data,
+                        type: 'get',
+                        success: function(data) {
+                            $('body').append(data);
+                        }
+                    });
+                }
+                
+
             }
         });
-    
-    
-        $.ajax({
-            url: 'https://cdn.togett.ru/button.html',
-            type: 'get',
-            success: function(data) {
-                $('body').append(data);
-            }
-        });
+
+        
     }
 
 });
